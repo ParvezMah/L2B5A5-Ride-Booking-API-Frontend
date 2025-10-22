d6306dcbf80911221149f5658ab0fbda865b38ee
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-22 08:31:03 +0600

    Added dark mode & Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/index.tsx";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
   </React.StrictMode>
 );
