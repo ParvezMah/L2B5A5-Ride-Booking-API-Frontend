5d90b2bf6f2f1ca24bcf27174c2f02ed8daa448c
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-22 09:41:59 +0600

    Implemented Login page

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,6 +12,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { ModeToggle } from "./ModeToggle";
+import { Link } from "react-router";
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
@@ -107,7 +108,7 @@ export default function Navbar() {
         <div className="flex items-center gap-2">
           <ModeToggle/>
           <Button asChild variant="ghost" size="sm" className="text-sm">
-            <a href="#">Sign In</a>
+            <Link to="/login">Sign In</Link>
           </Button>
           <Button asChild size="sm" className="text-sm">
             <a href="#">Get Started</a>
